e7a453b333d24550128f7e590208db7952e81fba
Adam Moore <phgprohunter@gmail.com>
2026-03-16 22:49:53 -0500

    w

--- gmpublisher/lua/autorun/tk_armored_commando.lua
+++
@@ -1,3 +0,0 @@
-player_manager.AddValidModel( "tk_armored_commando",						"models/player/armored_commando/armored_commando.mdl" ) 
-list.Set( "PlayerOptionsModel",  "tk_armored_commando",						"models/player/armored_commando/armored_commando.mdl" )
-player_manager.AddValidHands( "tk_armored_commando", "models/Player/hands/stormhands.mdl", 0, "00000000" )
